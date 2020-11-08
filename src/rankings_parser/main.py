5d999f3a1b429db4d6458b1fcfd44999f2d68233
ksemasinghe <a.kevin.schumacher@gmail.com>
2020-11-08 15:22:51 -0500

    Use dataclass, account for outliers

--- src/rankings_parser/main.py
+++ src/rankings_parser/main.py
@@ -1,5 +1,46 @@
 import click
 import csv
+import statistics
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Player:
+    name: str = ""
+    ratings: list = field(default_factory=list)
+
+    def average_rating(self):
+        if len(self.ratings) == 0:
+            return 0
+        else:
+            return round(sum(self.ratings) / len(self.ratings), 2)
+
+    def average_rating_within_std_dev(self):
+        if len(self.ratings) == 0:
+            return 0
+        else:
+            std_dev = self.std_dev()
+            unfiltered_average = self.average_rating()
+            lower_bound = unfiltered_average - std_dev
+            upper_bound = unfiltered_average + std_dev
+
+            filtered_ratings = [rating for rating in self.ratings if (upper_bound >= rating >= lower_bound)]
+            return round(sum(filtered_ratings) / len(filtered_ratings), 2)
+
+    def std_dev(self):
+        return statistics.stdev(self.ratings)
+
+    def __lt__(self, other):
+        return self.average_rating_within_std_dev() < other.average_rating_within_std_dev()
+
+    def __repr__(self):
+        name = f"{self.name}: "
+        avg = f"avg={self.average_rating()}. "
+        std_dev = f"std_dev={self.std_dev()}. "
+        avg_with_std_dev = f"avg_with_std_dev: {self.average_rating_within_std_dev()}. "
+        ratings = f"ratings={self.ratings}"
+        return f"{name}{avg_with_std_dev}{avg}{std_dev}{ratings}"
+
 
 RANKING_WEIGHT = {
     "S": 9,
@@ -20,31 +61,21 @@ Use like so on a command line interface
 """
 @click.command()
 @click.option('--file', type=click.File(), help="CSV file of rankings.", required=True)
-@click.option('--ordered', is_flag=True, default=False)
-def parse_rankings(ordered, file):
+def parse_rankings(file):
     csv_reader = csv.reader(file)
     players_list = next(csv_reader)[1:]
-    player_rankings_map = dict(zip(players_list, [[] for _ in players_list]))
+    players_map = dict(zip(players_list, [Player(name=player) for player in players_list]))
 
     for ratings_row in csv_reader:
         # Column 1 is the timestamp value
         # Update player mapping with weighted score for each letter rank
         for col_idx, rating in enumerate(ratings_row[1:]):
-            player_rankings_map[players_list[col_idx]].append(RANKING_WEIGHT[rating])
+            if RANKING_WEIGHT[rating] is not None:
+                players_map[players_list[col_idx]].ratings.append(RANKING_WEIGHT[rating])
 
-    # Filter out non letter ratings, and average them out for each player
-    for player, ratings in player_rankings_map.items():
-        filtered_ratings = [rating for rating in ratings if rating is not None]
-        player_rankings_map[player] = round(sum(filtered_ratings) / len(filtered_ratings), 2)
-
-    # If ordered is requested, we convert to a list of tuples, and order by average score
-    if ordered:
-        click.secho("Ordered average rankings", fg='green')
-        for p, r in _ordered_tuple_from(player_rankings_map):
-            click.secho(f"{p}: {r}", fg='blue')
-    else:
-        click.secho("Average rankings", fg='green')
-        click.secho(str(player_rankings_map), fg='blue')
+    click.secho("Ordered average rankings", fg='green')
+    for _, player_class in _ordered_tuple_from(players_map):
+        click.secho(str(player_class), fg='blue')
 
 
 def _ordered_tuple_from(rankings_map):
